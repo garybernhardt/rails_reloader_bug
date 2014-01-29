3ef8efb827aa4b3ef6300aa71926a84b79e3d19f
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-01-28 17:13:30 -0800

    add reloader callback

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  ActionDispatch::Reloader.to_prepare do
+    puts 'PREPARE'
+  end
 end
